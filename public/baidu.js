8c05a1623bb3265b730d9854b8a74a91f4c61633
Yidadaa <yidadaa@qq.com>
2023-08-16 00:12:17 +0800

    feat: 增加百度统计

---
+++ public/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8fd333d233eb5e123ab214408f826ad2";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
